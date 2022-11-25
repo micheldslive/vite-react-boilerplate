b5806c5ff3ecc42c76aeb9465f7b0bb84f62a018
Michel Domingos <michel.silva@dotgroup.com.br>
2022-11-25 06:42:00 -0300

    chore(config): adding eslint in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { EsLinter, linterPlugin } from 'vite-plugin-linter'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    linterPlugin({
+      include: ['./src/**/*.{ts,tsx}'],
+      linters: [new EsLinter({ configEnv })],
+    }),
+  ],
+}))
