b1936bdfa5d5adc5558c1944c7728a0ee8da25cd
Michel Domingos <michel.silva@dotgroup.com.br>
2022-11-25 08:06:05 -0300

    chore(setup): adding storybook

---
+++ .storybook/main.js
@@ -0,0 +1,22 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions'
+  ],
+  framework: '@storybook/react',
+  core: { builder: 'webpack5' },
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [new TsconfigPathsPlugin()]
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'import.meta.env': JSON.stringify(process.env),
+      }),
+    )
+    return config
+  },
+}
