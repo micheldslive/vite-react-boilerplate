d827a2785a9facd72c8505f0a8b48d2a8bf761e1
Michel Domingos <michel.silva@dotgroup.com.br>
2022-11-25 08:19:34 -0300

    chore(config): changing eslint import

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import pkg from 'vite-plugin-linter'
+import { EsLinter, linterPlugin } from 'vite-plugin-linter'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import svgrPlugin from 'vite-plugin-svgr'
-
-const { EsLinter, linterPlugin } = pkg
 
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => ({
